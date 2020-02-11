c73c1bb323df9eed6c5cb40bcf9ea00ffee042b1
Clayton Asada <casada94@gmail.com>
2020-02-10 16:56:54 -0800

    laying navigation and app ground work

--- app/src/main/java/com/example/mapp/MainActivity.java
+++ app/src/main/java/com/example/mapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.mapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolBar = (Toolbar) findViewById(R.id.toolBar);
+        setSupportActionBar(myToolBar);
+        myToolBar.setTitleTextColor(Color.parseColor("#FFFFFF"));
+        myToolBar.setTitle("Mapp");
+
     }
 }
