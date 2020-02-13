e44ce23b41409e89a3ad6cb9d9c8701f9d01b51d
Casada94 <40129886+Casada94@users.noreply.github.com>
2020-02-12 22:35:07 -0800

    made second fragment

--- app/src/main/java/com/example/mapp/MainActivity.java
+++ app/src/main/java/com/example/mapp/MainActivity.java
@@ -61,7 +61,7 @@ public class MainActivity extends AppCompatActivity {
         NavigationView navigationView = findViewById(R.id.nav_view);
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.schedule)
+                R.id.nav_home, R.id.secondHome)
                 .setDrawerLayout(drawer)
                 .build();
 
