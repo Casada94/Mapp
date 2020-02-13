9eaa66d5927295a0aebf0d94e1b03844def1cfde
Clayton Asada <casada94@gmail.com>
2020-02-12 18:54:55 -0800

    correcting errors from previous commit. app now opens and runs

--- app/src/main/java/com/example/mapp/MainActivity.java
+++ app/src/main/java/com/example/mapp/MainActivity.java
@@ -7,10 +7,13 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.view.ActionMode;
+import android.view.MenuItem;
 import android.view.View;
 
 import androidx.appcompat.widget.SearchView;
 import androidx.core.content.ContextCompat;
+import androidx.core.widget.TextViewCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -27,6 +30,9 @@ import android.view.Menu;
 import android.graphics.Color;
 //import android.os.Bundle;
 import android.view.Gravity;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -34,11 +40,14 @@ import com.google.android.material.navigation.NavigationView;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private boolean loggedIn = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         Toolbar myToolBar = (Toolbar) findViewById(R.id.toolBar);
         setSupportActionBar(myToolBar);
@@ -47,18 +56,55 @@ public class MainActivity extends AppCompatActivity {
         //myToolBar.setOverflowIcon(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_search));
 
 
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home)
+                R.id.nav_home, R.id.schedule)
                 .setDrawerLayout(drawer)
                 .build();
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView,navController);
+
+        Button signUp = (Button)navigationView.getHeaderView(0).findViewById(R.id.signUp);
+        Button login = (Button) navigationView.getHeaderView(0).findViewById(R.id.login);
+        TextView userInNav = (TextView) navigationView.getHeaderView(0).findViewById(R.id.whoIsIt);
+        TextView home = (TextView) navigationView.getHeaderView(0).findViewById(R.id.nav_home);
+
+
+
+
+        if(!loggedIn){
+            userInNav.setVisibility(View.GONE);
+
+
+            signUp.setVisibility(View.VISIBLE);
+            login.setVisibility(View.VISIBLE);
+        }
+        else{
+            userInNav.setText("Its a Me, Mario!");
+            userInNav.setVisibility(View.VISIBLE);
+            signUp.setVisibility(View.INVISIBLE);
+            login.setVisibility(View.INVISIBLE);
+
+
+        }
+
+
     }
+
+
+   /* @Override
+    public boolean onMenuItemClick(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.nav_home:
+                Toast.makeText(this,"clicked on home", Toast.LENGTH_LONG).show();
+        }
+        return true;
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -67,10 +113,10 @@ public class MainActivity extends AppCompatActivity {
         // Associate searchable configuration with the SearchView
         SearchManager searchManager =
                 (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        /*SearchView searchView =
+        SearchView searchView =
                 (SearchView) menu.findItem(R.id.search_button).getActionView();
         searchView.setSearchableInfo(
-                searchManager.getSearchableInfo(getComponentName()));*/
+                searchManager.getSearchableInfo(getComponentName()));
 
         return true;
     }
