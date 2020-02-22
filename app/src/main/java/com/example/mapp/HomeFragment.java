ab9150959bcd841b2f570a9caabcdcf9fb75062f
Clayton Asada <casada94@gmail.com>
2020-02-21 16:16:24 -0800

    image manipulation

--- app/src/main/java/com/example/mapp/HomeFragment.java
+++ app/src/main/java/com/example/mapp/HomeFragment.java
@@ -1,13 +1,24 @@
 package com.example.mapp;
 
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.util.FloatMath;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatCallback;
+import androidx.core.graphics.BitmapCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -15,6 +26,21 @@ import androidx.lifecycle.ViewModelProviders;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+
+    private ScaleGestureDetector mScaleGestureDetector;
+
+    private float mScaleFactor = 1.0f;
+    private ImageView map;
+
+    Matrix matrix = new Matrix();
+    Matrix savedMatrix = new Matrix();
+    PointF startPoint = new PointF();
+    PointF midPoint = new PointF();
+    float oldDist = 1f;
+    static final int NONE = 0;
+    static final int DRAG = 1;
+    static final int ZOOM = 2;
+    int mode = NONE;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -32,14 +58,96 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        map = root.findViewById(R.id.map);
 
+        mScaleGestureDetector = new ScaleGestureDetector(getContext(), new ScaleListener());
+
+        map.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event){
+                mScaleGestureDetector.onTouchEvent(event);
+
+                /*ImageView view = (ImageView) v;
+                System.out.println("matrix=" + savedMatrix.toString());
+                switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                    case MotionEvent.ACTION_DOWN:
+
+                        savedMatrix.set(matrix);
+                        startPoint.set(event.getX(), event.getY());
+                        mode = DRAG;
+                        break;
+
+                    case MotionEvent.ACTION_POINTER_DOWN:
+
+                        oldDist = spacing(event);
+
+                        if (oldDist > 10f) {
+                            savedMatrix.set(matrix);
+                            midPoint(midPoint, event);
+                            mode = ZOOM;
+                        }
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+
+                    case MotionEvent.ACTION_POINTER_UP:
+                        mode = NONE;
+
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        if (mode == DRAG) {
+                            matrix.set(savedMatrix);
+                            matrix.postTranslate(event.getX() - startPoint.x, event.getY() - startPoint.y);
+                        } else if (mode == ZOOM) {
+                            float newDist = spacing(event);
+
+                            if (newDist > 10f) {
+                                matrix.set(savedMatrix);
+                                float scale = newDist / oldDist;
+                                matrix.postScale(scale, scale, midPoint.x, midPoint.y);
+                            }
+                        }
+                        break;
+                }
+                view.setImageMatrix(matrix);*/
+
+                return true;
+            }
+/*
+            @SuppressLint("FloatMath")
+            private float spacing(MotionEvent event) {
+                float x = event.getX(0) - event.getX(1);
+                float y = event.getY(0) - event.getY(1);
+                return (float) Math.sqrt(x * x + y * y);
+            }
+
+            private void midPoint(PointF point, MotionEvent event) {
+                float x = event.getX(0) + event.getX(1);
+                float y = event.getY(0) + event.getY(1);
+                point.set(x / 2, y / 2);
+            }*/
+        });
+
+
+        //mScaleGestureDetector = new ScaleGestureDetector(map.getContext(), new ScaleListener());
 
 
 
         return root;
     }
 
-
-
+private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener{
+        @Override
+    public boolean onScale(ScaleGestureDetector scaleGestureDetector){
+            mScaleFactor *= scaleGestureDetector.getScaleFactor();
+            mScaleFactor = Math.max(0.1f,
+                    Math.min(mScaleFactor, 10.0f));
+            map.setScaleX(mScaleFactor);
+            map.setScaleY(mScaleFactor);
+            return true;
+        }
+}
 
 }
+
