b8fa0a5c901049e3039573c62a6a3bfb555a095a
ygvela <yvelasco598@gmail.com>
2020-03-02 23:16:53 -0800

    writes to database

--- app/src/main/java/com/example/mapp/LoginFragment.java
+++ app/src/main/java/com/example/mapp/LoginFragment.java
@@ -3,6 +3,7 @@ package com.example.mapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,10 +18,18 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firestore.v1.WriteResult;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class LoginFragment extends Fragment {
@@ -29,6 +38,7 @@ public class LoginFragment extends Fragment {
     private EditText username;
     private EditText password;
     private Button login;
+    private static String TAG= "EmailPassword";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -42,15 +52,14 @@ public class LoginFragment extends Fragment {
         login = root.findViewById(R.id.loginBtn);
 
         final FirebaseAuth mAuth = FirebaseAuth.getInstance();
-
-
-
+        final FirebaseFirestore database = FirebaseFirestore.getInstance();
+        final DocumentReference users_emails = database.document("users/users_emails");
 
         /* Sets the click functionality of the button*/
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String user = username.getText().toString();
+                final String user = username.getText().toString();
                 String pass = password.getText().toString();
 
                 /* Simple regex check for the formatting of the email**/
@@ -64,10 +73,45 @@ public class LoginFragment extends Fragment {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if(task.isSuccessful()){
+                                    Log.d(TAG, "signInWithEmail:success");
+                                    FirebaseUser curr_user = mAuth.getCurrentUser();
+                                    if(curr_user != null) {
+                                        Map<String, Object> user_email = new HashMap<>();
+                                        user_email.put("email", curr_user.getEmail());
+                                        /* document keeps track of user emails ONLY, for verifying later if email already exists */
+                                        users_emails.update(user_email).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Log.d(TAG, "Added user name and email");
+                                            }
+                                        }).addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.d(TAG, "Failed adding user name and email");
+                                            }
+                                        });
+                                        Map<String, Object> new_user = new HashMap<>();
+                                        new_user.put("email", curr_user.getEmail()); //fields each user will contain (we can add more, this was tester)
+                                        /* adds current user to collection */
+                                        database.collection("users").document(mAuth.getUid()).set(new_user)
+                                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                    @Override
+                                                    public void onSuccess(Void aVoid) {
+                                                        Log.w(TAG, "Added user to firestore.");
+                                                    }
+                                                })
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception e) {
+                                                        Log.w(TAG, "Error adding document", e);
+                                                    }
+                                                });
+                                    }
                                     Intent loggedIn = new Intent(getContext(), MainActivity.class);
                                     startActivity(loggedIn);
                                     getActivity().finish();
                                 }else{
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
                                     Toast.makeText(getActivity().getApplicationContext(), "Username or Password are incorrect", Toast.LENGTH_LONG).show();
                                 }
                             }
